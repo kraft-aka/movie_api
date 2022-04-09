043fee6ca23a05ddfd4e406a08f95418c6d80cff
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-04-09 19:43:21 +0200

    update and prettify the code

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,14 @@ const uuid = require('uuid');
 const users = require('../api/userslist');
 
 
-routerUsers.use(bodyParser.urlencoded({ extended: true}));
+routerUsers.use(bodyParser.urlencoded({ extended: true }));
 routerUsers.use(bodyParser.json());
 
 
 
-
-// create new user // CREATE
-routerUsers.post('/users', (req, res)=> {
+//------CREATE-----//
+// create new user // 
+routerUsers.post('/users', (req, res) => {
     const newUser = req.body;
 
     if (newUser.username) {
@@ -25,9 +25,10 @@ routerUsers.post('/users', (req, res)=> {
     }
 });
 
-// update user  //UPDATE
-routerUsers.put('/users/:id', (req, res)=> {
-    const {id} = req.params;
+//------UPDATE-----//
+// update user  
+routerUsers.put('/users/:id', (req, res) => {
+    const { id } = req.params;
     const updatedUser = req.body;
 
     let user = users.find(user => user.id === parseInt(id));
@@ -40,9 +41,10 @@ routerUsers.put('/users/:id', (req, res)=> {
     }
 });
 
-// add a movie  //CREATE
-routerUsers.post('/users/:id/:title', (req, res)=> {
-    const {id,title } = req.params;
+//------CREATE-----//
+// add a movie  
+routerUsers.post('/users/:id/:title', (req, res) => {
+    const { id, title } = req.params;
 
     let user = users.find(user => user.id === parseInt(id));
 
@@ -54,28 +56,30 @@ routerUsers.post('/users/:id/:title', (req, res)=> {
     }
 });
 
-// remove movie from favorite list   // DELETE
-routerUsers.delete('/users/:id/:title', (req, res)=> {
-    const {id,title } = req.params;
+//------DELETE-----//
+// remove movie from favorite list   
+routerUsers.delete('/users/:id/:title', (req, res) => {
+    const { id, title } = req.params;
 
     let user = users.find(user => user.id === parseInt(id));
 
     if (user) {
-        user.favoriteMovies = user.favoriteMovies.filter((deleteMovie)=> deleteMovie!== title);
+        user.favoriteMovies = user.favoriteMovies.filter((deleteMovie) => deleteMovie !== title);
         res.status(200).send(`${title} has been removed from ${id}'s favoriteMovies.`)
     } else {
         res.status(400).send('no such movie.');
     }
 });
 
-// remove movie from favorite list   // DELETE
-routerUsers.delete('/users/:id', (req, res)=> {
-    const {id } = req.params;
+//------DELETE-----//
+// remove movie from favorite list   
+routerUsers.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
 
     let user = users.find(user => user.id === parseInt(id));
 
     if (user) {
-        user = users.filter((user)=> user.id !== parseInt(id));
+        user = users.filter((user) => user.id !== parseInt(id));
         res.status(200).send(`user's ${id} has been deleted.`)
     } else {
         res.status(400).send('no such user or id.');
