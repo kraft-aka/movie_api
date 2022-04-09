5eea40c924acd092b4d70f0eb45a297aa314e37f
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-04-09 17:09:19 +0200

    create api users

---
+++ api/userslist.js
@@ -0,0 +1,21 @@
+let usersList = [
+    {
+        username: 'Mary',
+        id: 1,
+        favoriteMovies: []
+    },
+    {
+        username: 'John',
+        id: 2,
+        favoriteMovies: []
+    },
+    {
+        username: 'David',
+        id: 3,
+        favoriteMovies: [
+            'Aliens'
+        ]
+    }
+];
+
+module.exports = usersList;
