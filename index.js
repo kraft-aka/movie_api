757bde0ca3ad7a8530951f879dad6f93d1236c9e
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-04-09 19:42:06 +0200

    prettify the code

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ const app = express();
 // create a write stream (in append mode)
 // a "log.txt" file is created in root directory
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
-  flags: "a",
+    flags: "a",
 });
 
 // middlewares
 app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.static("public"));
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
 });
 
 app.use('/', router);
@@ -29,12 +29,12 @@ app.use('/', routerUsers);
 
 // Get requests
 app.get("/", (req, res) => {
-  res.send("Welcome to my Movie App!");
+    res.send("Welcome to my Movie App!");
 });
 
 
 // port listener
 app.listen(8080, () => {
-  console.log("Your application is listening on Port 8080");
+    console.log("Your application is listening on Port 8080");
 });
 
