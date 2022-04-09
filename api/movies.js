5534422919092bbc221b70f65d2cfe3b17887460
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-04-09 17:09:00 +0200

    update api movies

---
+++ api/movies.js
@@ -0,0 +1,65 @@
+// topTen Movies delete it later
+const movies = [
+    {
+        title: 'The Lord of the Rings',
+        genre: { name: 'Fantasy' },
+        director: { name:'Peter Jackson'},
+        id: 1
+    },
+    {
+        title: 'Aliens',
+        genre: { name: 'Action' },
+        director: { name:'James Cameron'},
+        id: 2
+    },
+    {
+        title: 'Toy Story',
+        genre: { name: 'Animated' },
+        director: { name:'John Lasseter'},
+        id: 3
+    },
+    {
+        title: 'Duck Soup',
+        genre: { name: 'Comedy' },
+        director: { name:'Leo McCarey'},
+        id: 4
+    },
+    {
+        title: 'Casablanca',
+        genre: { name: 'Romance' },
+        director: { name:'Michael Curtiz'},
+        id: 5
+    },
+    {
+        title: 'Rocky',
+        genre: { name: 'Sports' },
+        director: { name:'John G. Avildsen'},
+        id: 6
+    },
+    {
+        title: 'Good Fellas',
+        genre: { name: 'Crime' },
+        director: { name:'Martin Scorsese'},
+        id: 7
+    },
+    {
+        title: 'Taxi Driver',
+        genre: { name: 'Drama' },
+        director: { name:'Martin Scorsese'},
+        id: 8
+    },
+    {
+        title: 'High Noon',
+        genre: { name: 'Western' },
+        director: { name:'Fred Zinnemann'},
+        id: 9
+    },
+    {
+        title: 'Airport',
+        genre: { name: 'Disaster' },
+        director: { name:'George Seaton'},
+        id: 10
+    }
+];
+
+module.exports = movies;
