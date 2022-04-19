8f765478bd7593affdde27a96f050ef0650d1196
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-04-19 22:56:20 +0200

    update users CRUD

--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,17 @@ const routerUsers = express.Router();
 const bodyParser = require("body-parser");
 const uuid = require("uuid");
 const mongoose = require("mongoose");
-const Models = require("./models.js");
+const Models = require("../models");
 
-const users = require("../api/userslist");
-const { User } = require("../models");
+//const users = require("../api/userslist");
+//const { User } = require("../models");
 
 // references to model names created in models.js
 const Movies = Models.Movie;
 const Users = Models.User;
 
 // connect to DB
-mongoose.connect("mongodb://localhost:27017/myFlixDB", {
+mongoose.connect("mongodb://127.0.0.1/myFlixDB", {
   useNewURLParser: true,
   useUnifiedTopology: true,
 });
@@ -24,15 +24,6 @@ routerUsers.use(bodyParser.json());
 //------CREATE-----//
 // create new user //
 routerUsers.post("/users", (req, res) => {
-  /* const newUser = req.body;
- 
-     if (newUser.username) {
-         newUser.id = uuid.v4();
-         users.push(newUser);
-         res.status(201).json(newUser)
-     } else {
-         res.status(400).send('user needs a name')
-     }*/
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
@@ -86,17 +77,6 @@ routerUsers.get("/users/:Username", (req, res) => {
 //------UPDATE-----//
 // update user
 routerUsers.put("/users/:Username", (req, res) => {
-  /*const { id } = req.params;
-    const updatedUser = req.body;
-
-    let user = users.find(user => user.id === parseInt(id));
-
-    if (user) {
-        user.username = updatedUser.username;
-        res.status(200).json(user);
-    } else {
-        res.status(400).send('no such user.');
-    }*/
   Users.findOneAndUpdate(
     { Username: req.params.Username },
     {
@@ -122,18 +102,8 @@ routerUsers.put("/users/:Username", (req, res) => {
 //------CREATE-----//
 // add a movie
 routerUsers.post("/users/:Username/movies/:MovieID", (req, res) => {
-  /*const { id, title } = req.params;
-
-  let user = users.find((user) => user.id === parseInt(id));
-
-  if (user) {
-    user.favoriteMovies.push(title);
-    res.status(200).send(`${title} has been added to ${id}'s favoriteMovies.`);
-  } else {
-    res.status(400).send("no such movie.");
-  }*/
   Users.findOneAndUpdate({ Username: req.params.Username}, {
-      $push: { favoriteMovies: req.params.MovieID }
+      $push: { FavoriteMovies: req.params.MovieID }
   },
   { new: true },
   (err, updatedUser)=> {
@@ -148,36 +118,24 @@ routerUsers.post("/users/:Username/movies/:MovieID", (req, res) => {
 
 //------DELETE-----//
 // remove movie from favorite list
-routerUsers.delete("/users/:id/:title", (req, res) => {
-  const { id, title } = req.params;
-
-  let user = users.find((user) => user.id === parseInt(id));
-
-  if (user) {
-    user.favoriteMovies = user.favoriteMovies.filter(
-      (deleteMovie) => deleteMovie !== title
-    );
-    res
-      .status(200)
-      .send(`${title} has been removed from ${id}'s favoriteMovies.`);
-  } else {
-    res.status(400).send("no such movie.");
-  }
+routerUsers.delete("/users/:Username/movies/:MovieID", (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username}, {
+      $pull: { FavoriteMovies: req.params.MovieID }
+  },
+  { new: true },
+  (err, updatedUser)=> {
+      if (err) {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+      } else {
+          res.json(updatedUser);
+      }
+  });
 });
 
 //------DELETE-----//
 // remove user 
 routerUsers.delete("/users/:Username", (req, res) => {
- /* const { id } = req.params;
-
-  let user = users.find((user) => user.id === parseInt(id));
-
-  if (user) {
-    user = users.filter((user) => user.id !== parseInt(id));
-    res.status(200).send(`user's ${id} has been deleted.`);
-  } else {
-    res.status(400).send("no such user or id.");
-  }*/
   Users.findOneAndRemove({ Username: req.params.Username})
     .then((user)=> {
         if(!user) {
