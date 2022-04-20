7419e4bd01135215fda82ba588f0903cd7d21924
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-04-20 13:36:24 +0200

    delete unused references

--- index.js
+++ index.js
@@ -3,21 +3,10 @@ const res = require("express/lib/response");
 const morgan = require("morgan");
 const fs = require("fs");
 const path = require("path");
-const router = require('./routes/movies');
-const routerUsers = require('./routes/users');
-const movies = require('./api/movies');
-const users = require('./api/userslist');
-const swaggerUi = require('swagger-ui-express'),// init swagger
-swaggerDocument = require('./swagger.json');
-const mongoose = require('mongoose');
-const Models = require('./models.js');
-
-// references to model names created in models.js
-const Movies = Models.Movie;
-const Users = Models.User;
-
-// connect to DB 
-mongoose.connect('mongodb://127.0.0.1/myFlixDB', {useNewURLParser: true, useUnifiedTopology: true});
+const router = require("./routes/movies");
+const routerUsers = require("./routes/users");
+const swaggerUi = require("swagger-ui-express"), // init swagger
+  swaggerDocument = require("./swagger.json");
 
 // init app
 const app = express();
@@ -25,37 +14,29 @@ const app = express();
 // create a write stream (in append mode)
 // a "log.txt" file is created in root directory
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
-    flags: "a",
+  flags: "a",
 });
 
 // middlewares
 app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.static("public"));
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send("Something broke!");
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
-app.use('/', router);
-app.use('/', routerUsers);
+app.use("/", router);
+app.use("/", routerUsers);
 
-app.use(
-    '/api-docs',
-    swaggerUi.serve, 
-    swaggerUi.setup(swaggerDocument)
-  );
-
-
-
+// swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Get requests
 app.get("/", (req, res) => {
-    res.send("Welcome to my Movie App!");
+  res.send("Welcome to my Movie App!");
 });
-
 
 // port listener
 app.listen(8080, () => {
-    console.log("Your application is listening on Port 8080");
+  console.log("Your application is listening on Port 8080");
 });
-
