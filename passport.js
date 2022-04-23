63863f3729d14859289d627aa68f58d930a4cc07
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-04-23 12:43:40 +0200

    add comment lines

--- passport.js
+++ passport.js
@@ -8,12 +8,13 @@ let Users = Models.User;
 let JWTStrategy = passportJWT.Strategy;
 let ExtractJWT = passportJWT.ExtractJwt;
 
+// basic HTTP authentication for login requests
 passport.use(new LocalStrategy({
     usernameField: 'Username',
     passwordField: 'Password'
 }, (username, password, callback) => {
     console.log(username + ' '+ password);
-    Users.findOne({ Username: username }, (error, user)=> {
+    Users.findOne({ Username: username }, (error, user)=> { // Mongoose used to check the username in DB
         if (error) {
             console.log(error);
             return callback(error);
@@ -29,6 +30,7 @@ passport.use(new LocalStrategy({
     });
 }));
 
+// authentication of users based on JWT
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey: 'your_jwt_secret'
