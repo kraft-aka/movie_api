07e181ad3e771cff44b0a17af25d3ebe4ae8a5a7
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-04-26 15:05:36 +0200

    create hashPassword and validatePassword
    Import bcrypt to create two function for hashing passwords and validating passowrds.
    Add comment lines

--- models.js
+++ models.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+// schema for movies
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
     Genre: {
@@ -15,6 +17,7 @@ let movieSchema = mongoose.Schema({
     Featured: Boolean
 });
 
+// schema for users
 let userSchema = mongoose.Schema({
     Username: {type: String, required: true},
     Password: {type: String, required: true},
@@ -23,8 +26,21 @@ let userSchema = mongoose.Schema({
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
+// function to hash a password
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+// function to validate users's password
+userSchema.methods.validatePassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
+
+// define models variables
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
 
+
+// export models
 module.exports.Movie = Movie;
 module.exports.User = User;
