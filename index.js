d44b5d887e07ff4126b7fedb92a022ba64963c0c
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-04-26 13:48:02 +0200

    feat: integrate cors moule
    cors module is integrated and code for allowed domains defined.

--- index.js
+++ index.js
@@ -10,10 +10,27 @@ const swaggerUi = require("swagger-ui-express"); // init swagger
 const swaggerDocument = require("./swagger.json");
 const passport = require('passport');
 require('./passport');
+const cors = require('cors');
 
 
 // init app
 const app = express();
+
+// define a list of allowed domains
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+// set a cors for allowed domains
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      // if a specific origin isn't found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null,true);
+  }
+}));
 
 // init body parser
 app.use(bodyParser.urlencoded({extended: true}));
