b9cc5890cfbab3a5d00f023dca1846c1fae14966
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-05-01 14:25:10 +0200

    add role to user

--- models.js
+++ models.js
@@ -22,6 +22,7 @@ let userSchema = mongoose.Schema({
   Username: { type: String, required: true },
   Password: { type: String, required: true },
   Email: { type: String, required: true },
+  role: { type: String, default: "User", enums: ["User", "Admin"]},
   Birthday: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
 });
