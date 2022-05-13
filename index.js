616a4d21793db3a5a16c9598cd19b6e49db42976
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-05-13 10:04:56 +0200

    comment out cors

--- index.js
+++ index.js
@@ -42,22 +42,22 @@ let allowedOrigins = [
   "http://localhost:1234"
 ];
 
-// // set a cors for allowed domains
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // if a specific origin isn't found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn't allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// set a cors for allowed domains
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // if a specific origin isn't found on the list of allowed origins
+//         let message =
+//           "The CORS policy for this application doesn't allow access from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 // init body parser
 app.use(bodyParser.urlencoded({ extended: true }));
