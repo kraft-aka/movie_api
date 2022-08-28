27fb92df620ffc43578cd4fe935485741d218dc0
[kraft-aka] <57872789+kraft-aka@users.noreply.github.com>
2022-08-28 16:30:39 +0200

    add JSDoc comments

--- auth.js
+++ auth.js
@@ -5,17 +5,31 @@ const routerLogin = express.Router();
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
 
-require("./passport"); // local passport file
+//local passport file
+require("./passport");
 
+/**
+ * Create JWT token
+ * @param {Object} user
+ * @returns {Object} user
+ */
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
-    subject: user.Username, // this is the username I am encodung in the JWT
-    expiresIn: "7d", // this specifies that the token will expire in 7 days
-    algorithm: "HS256", // this is the algorithm used to "sign" or encode the values of the JWT
+    subject: user.Username,
+    //this specifies that the token will expire in 7 days
+    expiresIn: "7d",
+    //this is the algorithm used to "sign" or encode the values of the JWT
+    algorithm: "HS256",
   });
 };
 
-// POST login
+/**
+ * User login, JWT generated with HTTP POST
+ * @function [path]/login
+ * @param {any} routerActors
+ * @returns {Object} user
+ * @requires passport
+ */
 
 routerLogin.post("/login", (req, res) => {
   passport.authenticate("local", { session: false }, (error, user, info) => {
--- routes/actors.js
+++ routes/actors.js
@@ -1,37 +1,37 @@
-const express = require('express');
+const express = require("express");
 const routerActors = express.Router();
-const mongoose = require('mongoose');
-const Models = require('../models');
-const passport = require('passport');
+const Models = require("../models");
+const passport = require("passport");
 const bodyParser = require("body-parser");
 
-const { Actors} = Models;
+/**
+ * Initialize a document variable from DB
+ */
+const { Actors } = Models;
 
-
-routerActors.use(bodyParser.urlencoded({extended:true}));
+routerActors.use(bodyParser.urlencoded({ extended: true }));
 routerActors.use(bodyParser.json());
 
-// init auth
-let auth = require("../auth");
-
-
-// get list of actors
-
-routerActors.get('/.actors', passport.authenticate("jwt", { session: false }),(res,req) => {
-    Actors.find()
-    .then((actors) => res.status(201).json(actors)
-    .catch((error)=> {
-        console.log(error);
-        res.status(500).send('Error '+ error);
-    }))   
-});
-
-
-// // get an actor by id
-// routerActors.get('/actors/:ActorID', (req, res) => {
-//     console.log(req.params.ActorID);
-// });
-
+/**
+ * Returns all actors from the API with HTTP method GET
+ * @function [path]/actors
+ * @returns {Object[]} actors
+ * @requires passport
+ */
+routerActors.get(
+  "/actors",
+  passport.authenticate("jwt", { session: false }),
+  (res, req) => {
+    Actors.find().then((actors) =>
+      res
+        .status(201)
+        .json(actors)
+        .catch((error) => {
+          console.log(error);
+          res.status(500).send("Error " + error);
+        })
+    );
+  }
+);
 
 module.exports = routerActors;
-
